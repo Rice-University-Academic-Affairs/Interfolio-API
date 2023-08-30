3b2716bbc7b2e3ffc4c0cf6bd66c6f33ddf2c6d9
Cody VanZandt <cody.a.vanzandt@gmail.com>
2023-08-30 11:52:35 -0500

    extracting header and api building out of get_units

--- src/interfolio_far.py
+++ src/interfolio_far.py
@@ -14,38 +14,35 @@ class InterfolioFAR:
 
     def get_units(self, data: str, unit_id=None, limit: int = None, offset: int = None):
         """
-        :param unit_id: ID of academic unit
         :param data: count, summary, or detailed
+        :param unit_id: ID of academic unit
         :param limit: limits number returned
         :param offset: offset for paginating values
         https://www.faculty180.com/swagger/ui/index.html#section/Standard-GET-Parameters-Used
         """
-        timestamp = self._create_timestamp()
         api_endpoint = "/units"
         api_method = "GET"
         query_params = f"?data={data}"
-        message = self._build_message(api_method, timestamp, api_endpoint)
-        signature = self._generate_signature(message, self.config.private_key)
-        auth_header = self._build_header(signature)
-        api_url = self.config.host + api_endpoint + query_params
-        headers = {
-            "TimeStamp": timestamp,
-            "Authorization": auth_header,
-            "INTF-DatabaseID": self.config.database_id,
-        }
+
+        headers = self._build_headers(api_endpoint, api_method)
+        api_url = self._build_api_url(api_endpoint, query_params)
+
         response = requests.get(api_url, headers=headers)
         response_text = response.text
-        print("Response:", response_text)
+        return response_text
 
-    def _build_message(self, api_method, timestamp, request_string):
-        return f"{api_method}\n\n\n{timestamp}\n{request_string}"
+    def _build_api_url(self, api_endpoint, query_params):
+        return self.config.host + api_endpoint + query_params
 
-    @staticmethod
-    def _generate_signature(message, private_key):
-        signature_bytes = hmac.new(
-            private_key.encode(), message.encode(), hashlib.sha1
-        ).digest()
-        return base64.b64encode(signature_bytes).decode()
+    def _build_headers(self, api_endpoint, api_method):
+        timestamp = self._create_timestamp()
+        message = self._build_message(api_endpoint, api_method, timestamp)
+        signature = self._build_signature(message)
+        return {
+            "TimeStamp": self._create_timestamp(),
+            "Authorization": self._build_authentication_header(signature),
+            "INTF-DatabaseID": self.config.database_id,
+        }
 
     @staticmethod
     def _create_timestamp():
@@ -53,5 +50,14 @@ class InterfolioFAR:
             "%Y-%m-%d %H:%M:%S"
         )
 
-    def _build_header(self, signature):
+    def _build_message(self, api_endpoint, api_method, timestamp):
+        return f"{api_method}\n\n\n{timestamp}\n{api_endpoint}"
+
+    def _build_signature(self, message):
+        signature_bytes = hmac.new(
+            self.config.private_key.encode(), message.encode(), hashlib.sha1
+        ).digest()
+        return base64.b64encode(signature_bytes).decode()
+
+    def _build_authentication_header(self, signature):
         return f"INTF {self.config.public_key}:{signature}"
--- tests/test_interfolio_far.py
+++ tests/test_interfolio_far.py
@@ -1,7 +1,16 @@
 import pytest
+import hmac
+import hashlib
+import base64
 from src.interfolio_far import InterfolioFAR
-from constants import API_KEYS
 from freezegun import freeze_time
+
+
+@pytest.fixture
+def far():
+    return InterfolioFAR(
+        database_id="id", public_key="public_key", private_key="private_key"
+    )
 
 
 class TestInterfolioFAR:
@@ -16,6 +25,21 @@ class TestInterfolioFAR:
         assert far_api.config.private_key == private_key
         assert far_api.config.database_id == database_id
 
+    def test__build_signature(self, far):
+        message = "message"
+        signature_bytes = hmac.new(
+            far.config.private_key.encode(), message.encode(), hashlib.sha1
+        ).digest()
+        signature = base64.b64encode(signature_bytes).decode()
+        assert far._build_signature(message) == signature
+
     @freeze_time("1994-12-02 10:00:00")
     def test__create_timestamp(self):
         assert InterfolioFAR._create_timestamp() == "1994-12-02 10:00:00"
+
+    def test__build_authentication_header(self, far):
+        signature = "signature"
+        assert (
+            far._build_authentication_header(signature)
+            == f"INTF {far.config.public_key}:{signature}"
+        )
